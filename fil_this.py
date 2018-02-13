9feb064a446efe5660322f8bf03fbfbb5f690916
devanshkv <devansh.kv@gmail.com>
2018-02-13 14:42:52 -0500

    added %RAM useage, now runs for full files.

--- fil_this.py
+++ fil_this.py
@@ -10,12 +10,18 @@ sys.path.append("/opt/pulsar/src/PRESTOv2.7.13/lib/python/")
 import filterbank as fb 
 import sigproc
 
+# written by Devansh Agarwal
+# Feb 12, 2018
+# mail: devanshkv@gmail.com
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--all_files', metavar="path", type=str,\
         help="Path to files to be merged; enclose in quotes, accepts * as wildcard for directories or filenames")
+parser.add_argument('-m', '--mem', type=float, help = "% RAM you want to use [0-1], def = 0.1", default = 0.1)
 
 args = parser.parse_args()
 files = glob.glob(args.all_files)
+mem_percent = float(args.mem)
 
 if not files:
     print('File(s) does not exist: ' + args.all_files) #, file=sys.stderr)
@@ -23,15 +29,45 @@ for file in files:
     print('Running using files: ' + file)
 
 
+# Get RAM info
+meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
+mem_mib = meminfo['MemTotal']/1024
+
+# Get nrows corresponding to a given fraction of RAM
+# NOTE: Even if you give -m 1, I will scale it down to 75%
+# Hence the 0.75 factor
+# 100*25*7*4*20*4e-6
+# 100 samples per row
+# 25 chanels per bank
+# 7 beams
+# 4 pols
+# 20 bank files
+# 4e-6 sizeof(32 bit float)
+if 0 < mem_percent <= 1:
+     nrows=int(0.75*mem_mib*mem_percent/(100*25*7*4*20*4e-6))
+else:
+    raise ValueError("percent RAM usage cannot be > 1")
+
+# Get num_runs
+
+#hdu=fits.open(file)
+data, header = fits.getdata(file, header=True)
+tot_rows=int(header["NAXIS2"])
+print "total rows : ", tot_rows
+
+nrow_list=list(range(0,tot_rows, nrows))
+nrow_list.append(tot_rows)
+
+
 # This generates all the 20 bank label from A to T.
 bank_labels = [chr(i) for i in range(ord('A'),ord('T')+1)]
 
-# Do this many rows
-nrows=1000
-
 # instead of filling zeores later, make a zeros array and fill with useful
 # values later in the for loop
-band_pass = np.zeros(shape=(nrows,100,500,4,7))
+band_pass = np.zeros(shape=(nrows,100,500,4,7), dtype=np.float32)
+# the last few rows will be left out so last_pass will contain them
+last_pass = np.zeros(shape=(nrow_list[-1]-nrow_list[-2],100,500,4,7), dtype=np.float32)
+last_pass_flag=False
 
 # the following chunk makes up the vector to fix the frequency structure.
 # as the frequencies are not contigous the following makes arrays as 
@@ -60,7 +96,7 @@ header=fb.read_header(fil_file)
 for i in range(7): #out_file_names:
     fb.create_filterbank_file(out_file_names[i],header=header[0],nbits=32)
 
-for rows in range(5):
+for rows in range(len(nrow_list)-1):
     # to loop through all bank frequecies
     bank_freq_index=0
     # loops through all banks
@@ -73,15 +109,23 @@ for rows in range(5):
                 hdu=fits.open(file)
                 data=hdu[1].data['DATA']
                 # the data is this is 100 (time samples) X 25 (chans) X 4 (pol)  X 7 (beams)
-                band_pass[:,:,freq_lists[bank_freq_index],:,:]=data[rows*nrows:(rows+1)*nrows,:].reshape(nrows,100,25,4,7)
+                # if running for last few rows use last_pass, and raise the flag!
+                try:
+                    band_pass[:,:,freq_lists[bank_freq_index],:,:]=data[nrow_list[rows]:nrow_list[rows+1],:].reshape(nrows,100,25,4,7)
+                except ValueError:
+                    last_pass[:,:,freq_lists[bank_freq_index],:,:]=data[nrow_list[rows]:nrow_list[rows+1],:].reshape(nrows,100,25,4,7)
+                    last_pass_flag=True
                 hdu.close()
         bank_freq_index+=1
 
-    print "writing rows : ", rows*nrows, "to" ,(rows+1)*nrows
+    print "writing rows : ", nrow_list[rows], "to" , nrow_list[rows+1]
     # write the 7 beams
+    if last_pass_flag:
+        band_pass = last_pass
     for file_num in range(7):
         file=fb.FilterbankFile(out_file_names[file_num], mode='append')
         file.append_spectra((band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500))
+
 
 print "Closing Files"
 for file_num in range(7):
