f67b8b14929d02adeb35f4b0a6118a3f9a055a69
devanshkv <devansh.kv@gmail.com>
2018-02-14 17:15:33 -0500

    bandflip and (almost complete) header

--- fil_this.py
+++ fil_this.py
@@ -5,6 +5,8 @@ import numpy as np
 import sys
 import glob
 import argparse
+import struct
+import fml
 # change presto path here
 sys.path.append("/opt/pulsar/src/PRESTOv2.7.13/lib/python/")
 import filterbank as fb 
@@ -14,75 +16,22 @@ import sigproc
 # Feb 12, 2018
 # mail: devanshkv@gmail.com
 
-# A function to add a SigProc Header
-
-def getsigprocheader(file):
-
-  telescope_ids = {"Fake": 0, "Arecibo": 1, "Ooty": 2, "Nancay": 3,
-                 "Parkes": 4, "Jodrell": 5, "GBT": 6, "GMRT": 7,
-                 "Effelsberg": 8}
-
-  machine_ids = {"WAPP": 0, "PSPM": 1, "Wapp": 2,"AOFTM": 3,
-               "BCPM1": 4, "FLAGBF": 5, "SCAMP": 6,
-               "GBT Pulsar Spigot": 7, "SPIGOT": 7, "PUPPI": 8 ,
-               "GUPPI": 9,"PA":10,"VEGAS": 11}
-
-
-  hdulist = fits.open(file)
-
-
-  def prep_string(string):
-    return struct.pack('i', len(string))+string
-
-  def prep_double(name, value):
-    return prep_string(name)+struct.pack('d', float(value))
-
-  def prep_int(name, value):
-    return prep_string(name)+struct.pack('i', int(value))
-
-  hdr = prep_string("HEADER_START")
-  hdr += prep_int("telescope_id", 6)
-  hdr += prep_int("machine_id",5)
-  hdr += prep_int("data_type", 1) # 1 = filterbank, 2 = timeseries
-
-  source =hdulist[0].header['OBJECT']
-
-#TODO: parse RA DEC from the ancillary fits files
-
-  hdr += prep_string("source_name")
-  hdr += prep_string(source)
-  hdr += prep_int("barycentric", 1)
-  hdr += prep_int("pulsarcentric", 0)
-  hdr += prep_double("src_raj",12345)
-  hdr += prep_double("src_dej", 12345)
-  hdr += prep_int("nbits", 32)
-  hdr += prep_int("nifs", 1)
-
-  hdr += prep_int("nchans", 500)
-  hdr += prep_double("fch1", 1325.0)
-  hdr += prep_double("foff",-0.30318)
-
-  MJD = hdulist[5].header['UTDSTART']
-
-  hdr += prep_double("tsamp",0.000130)
-
-  return hdr
-
-
-# Main code begins here
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--all_files', metavar="path", type=str,\
-        help="Path to files to be merged; enclose in quotes, accepts * as wildcard for directories or filenames")
-parser.add_argument('-m', '--mem', type=float, help = "% RAM you want to use [0-1], def = 1", default = 1.0)
-parser.add.argument('-fb' '--flipband', type=int,help = "Flag to flip the band pass while storing to filterbank",default=1)
+        help="Path to files to be merged; enclose in quotes, accepts\
+        * as wildcard for directories or filenames", required=True)
+parser.add_argument('-m', '--mem', type=float, help = "% RAM you \
+        want to use [0-1] (def=1)", default = 1.0, required=False)
+parser.add_argument('--noflip', nargs='?', const=False, default=True,\
+        help='Flag to flip the band pass while storing to filterbank (def=True)',\
+        required=False)
 
 args = parser.parse_args()
 all_files = glob.glob(args.all_files)
 mem_percent = float(args.mem)
-flag_flip = int(args.flipband)
-
+flip = args.noflip
+print "flip : ", flip
 # if the files don't exist exit
 if not all_files:
     print('File(s) does not exist: ' + args.all_files) #, file=sys.stderr)
@@ -95,6 +44,7 @@ for file in all_files:
     junk, header = fits.getdata(file, header=True)
     junk=None
     row_nos.append(int(header["NAXIS2"]))
+    MJD = float(header['STRTDMJD'])
 
 # get the mode
 tot_rows = max(row_nos, key=row_nos.count)
@@ -103,13 +53,14 @@ tot_rows = max(row_nos, key=row_nos.count)
 files=[]
 for idx, file in enumerate(all_files):
     if row_nos[idx]==tot_rows:
-        print('Running using files: ' + file)
+        #print('Running using files: ' + file)
         files.append(file)
+print "Using ", len(files), "banks"
 
 
 # Get RAM info
-meminfo = dict((i.split()[0].rstrip(':'),int(i.split()[1])) for i in open('/proc/meminfo').readlines())
-mem_mib = meminfo['MemTotal']/1024
+
+mem_mib = fml.FreeMemLinux(unit='MB').user_free # meminfo['MemTotal']/1024
 
 
 # Get nrows corresponding to a given fraction of RAM
@@ -123,7 +74,7 @@ mem_mib = meminfo['MemTotal']/1024
 # 20 bank files
 # 4e-6 sizeof(32 bit float)
 if 0 < mem_percent <= 1:
-     nrows=int(0.5*mem_mib*mem_percent/(100*25*7*4*20*4e-6))
+     nrows=int(0.75*mem_mib*mem_percent/(100*25*7*4*20*4e-6))
 else:
     raise ValueError("percent RAM usage cannot be > 1")
 
@@ -164,17 +115,34 @@ for _ in range(20):
 # dummy files to write_out_stuff
 out_file_names=["BF_beam_%i.fil" %i for i in range(7)]
 
-# a function that creates a sigproc header from the metadata in the fits files
-header = getsigprocheader(files[0])
+# make header here
+hdrdict={}
+hdrdict["telescope_id"]=int(6)
+hdrdict["machine_id"]=int(5)
+hdrdict["data_type"]=int(1)
+hdrdict["source_name"]=str("source")
+hdrdict["barycentric"]=int(1)
+hdrdict["pulsarcentric"]=int(0)
+hdrdict["src_raj"]=float(12345)
+hdrdict["src_dej"]=float(12345)
+hdrdict["nbits"]=int(32)
+hdrdict["nifs"]=int(1)
+hdrdict["nchans"]=int(500)
+hdrdict["fch1"]=float(1535.0)
+hdrdict["foff"]=float(-0.30318)
+hdrdict["tstart"]=float(MJD)
+hdrdict["tsamp"]=float(0.000130)
 
 
-# open files to dump header
+## open files to dump header
 for i in range(7): #out_file_names:
-    fb.create_filterbank_file(out_file_names[i],header=header[0],nbits=32)
-
-for rows in range(len(nrow_list)-1):
+    fb.create_filterbank_file(out_file_names[i],header=hdrdict)
+#
+for rows in range(len(nrow_list)-2,len(nrow_list)-1):
     print "Manipulating rows : ", nrow_list[rows], "to" , nrow_list[rows+1]
     if rows==len(nrow_list)-2:
+        # when doing the last bit make the zeros array
+        # set the flag and empty the memory taken by band_pass
         last_pass = np.zeros(shape=(lrows,100,500,4,7), dtype=np.float32)
         last_pass_flag=True
         band_pass = None
@@ -200,25 +168,19 @@ for rows in range(len(nrow_list)-1):
 
     print "writing rows : ", nrow_list[rows], "to" , nrow_list[rows+1]
     # write the 7 beams
-   # if last_pass_flag:
-   #     band_pass = last_pass
+    # band flip if needed
     for file_num in range(7):
         file=fb.FilterbankFile(out_file_names[file_num], mode='append')
         if last_pass_flag:
-          if(flag_flip==1):
-            last_pass_f = (last_pass[:,:,:,1,file_num]+last_pass[:,:,:,0,file_num]).reshape(100*lrows,500)
-            last_pass_flip = np.flip(last_pass_f,axis=1)
-            file.append_spectra(last_pass_flip)
-          else:
-            file.append_spectra((last_pass[:,:,:,1,file_num]+last_pass[:,:,:,0,file_num]).reshape(100*lrows,500))
+            if flip:
+                file.append_spectra((last_pass[:,:,:,1,file_num]+last_pass[:,:,:,0,file_num]).reshape(100*lrows,500)[:,::-1])
+            else:
+                file.append_spectra((last_pass[:,:,:,1,file_num]+last_pass[:,:,:,0,file_num]).reshape(100*lrows,500))
         else:
-          if (flag_flip==1):
-            band_pass_f = (band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500)
-            band_pass_flip = np.flip(band_pass_f,axis-1)
-            file.append_spectra((band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500))
-        
-          else:
-            file.append_spectra((band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500))
+            if flip:
+                file.append_spectra((band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500)[:,::-1])
+            else:
+                file.append_spectra((band_pass[:,:,:,1,file_num]+band_pass[:,:,:,0,file_num]).reshape(100*nrows,500))
             
 
 print "Closing Files"
